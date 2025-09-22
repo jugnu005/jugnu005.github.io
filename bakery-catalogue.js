06888c40d6d733c9b500ee2493a4ce70157b14e6
jugnu005 <jugnu005@users.noreply.github.com>
2025-09-22 17:58:16 -0500

    Create bakery-catalogue.js

---
+++ bakery-catalogue.js
@@ -0,0 +1,26 @@
+const bakeryCatalogue = [
+  {
+    category: "Cakes",
+    items: [
+      { name: "Chocolate Fudge Cake", image: "images/chocolate_fudge.jpg", price: "$15" },
+      { name: "Red Velvet", image: "images/red_velvet.jpg", price: "$18" },
+      { name: "Lemon Drizzle", image: "images/lemon_drizzle.jpg", price: "$14" }
+    ]
+  },
+  {
+    category: "Pastries",
+    items: [
+      { name: "Croissant", image: "images/croissant.jpg", price: "$3" },
+      { name: "Apple Turnover", image: "images/apple_turnover.jpg", price: "$4" },
+      { name: "Cream Puff", image: "images/cream_puff.jpg", price: "$3.50" }
+    ]
+  },
+  {
+    category: "Breads",
+    items: [
+      { name: "Sourdough", image: "images/sourdough.jpg", price: "$6" },
+      { name: "Baguette", image: "images/baguette.jpg", price: "$5" },
+      { name: "Multigrain Loaf", image: "images/multigrain.jpg", price: "$7" }
+    ]
+  }
+];
